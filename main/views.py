1518e9129dad1e10592300df739865514bf850b2
KirillMelanich <kirillmelanich@gmail.com>
2024-09-27 17:47:28 +0100

    Create Index page with styles and icons

--- main/views.py
+++ main/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 def index(request):
     context = {
         "title": "Home",
-        "content": "Main page of the shop"
+        "content": "Main page of the shop - HOME",
+        "list": [1, 2, 3, 4, 5],
+        "dict": {"a": 1, "b": 2, "c": 3},
     }
     return render(request, "main/index.html", context=context)
 
