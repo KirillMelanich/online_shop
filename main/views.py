655150a976a298a730b18c0723507380a04a18d5
KirillMelanich <kirillmelanich@gmail.com>
2024-09-30 21:12:00 +0100

    Create custom tag for categories

--- main/views.py
+++ main/views.py
@@ -4,12 +4,10 @@ from goods.models import Categories, Products
 
 
 def index(request):
-    categories = Categories.objects.all()
 
     context = {
         "title": "Home",
         "content": "Furniture store home page",
-        "categories": categories,
     }
     return render(request, "main/index.html", context)
 
