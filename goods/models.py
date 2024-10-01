4d4951a99e0dbac88f8f82010ff2d7a8b3c1c94f
KirillMelanich <kirillmelanich@gmail.com>
2024-10-01 14:12:02 +0100

    Change product controller from id to slug view

--- goods/models.py
+++ goods/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True)
-    slug = models.CharField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    slug = models.CharField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
 
     class Meta:
         db_table = "category"
@@ -16,13 +18,19 @@ class Categories(models.Model):
 
 class Products(models.Model):
     name = models.CharField(max_length=150)
-    slug = models.CharField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    slug = models.CharField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="goods_images", blank=True, null=True)
     price = models.DecimalField(default=0, max_digits=7, decimal_places=2)
-    discount = models.DecimalField(default=0, max_digits=4, decimal_places=2, verbose_name="Discount in %")
+    discount = models.DecimalField(
+        default=0, max_digits=4, decimal_places=2, verbose_name="Discount in %"
+    )
     quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name="Category")
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Category"
+    )
 
     class Meta:
         db_table = "product"
@@ -33,12 +41,9 @@ class Products(models.Model):
         return self.name
 
     def display_id(self):
-        return f'{self.id:05}'
+        return f"{self.id:05}"
 
     def sell_price(self):
         if self.discount:
             return round(self.price - (self.price * self.discount / 100), 2)
         return self.price
-
-
-
--- goods/views.py
+++ goods/views.py
@@ -6,10 +6,13 @@ def catalog(request):
     goods = Products.objects.all()
     context = {
         "title": "Home - Catalog",
-        'goods': goods,
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context=context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+    context = {"product": product}
+    return render(request, "goods/product.html", context=context)
