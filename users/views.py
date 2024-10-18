66f3bdfc407f8d902d7ac360f0e900ba4b4fb055
KirillMelanich <kirillmelanich@gmail.com>
2024-10-18 19:20:36 +0100

    Add delete cart options

--- users/views.py
+++ users/views.py
@@ -18,8 +18,9 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                messages(success(request, f"{username}, you are logged in"))
-                if request.POST.get("next"):
+                messages(success(request, f"{username}, You are logged in"))
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse("user:logout"):
                     return HttpResponseRedirect(request.POST.get("next"))
                 return HttpResponseRedirect(reverse("main:index"))
     else:
